6be5bd0bd71fc1223e19198e3d31f092b6f1eda1
Yoseph Prasetyo Lembang <yoseph.prasetyo@esb.co.id>
2022-06-13 10:56:08 +0700

    init commmit

---
+++ models/Invoice.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "invoice".
+ *
+ * @property int $id
+ * @property string $no
+ * @property string $date
+ * @property int|null $created_at
+ * @property int|null $created_by
+ * @property int|null $updated_at
+ * @property int|null $updated_by
+ */
+class Invoice extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'invoice';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['date'], 'required'],
+            [['date'], 'safe'],
+            [['created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            [['no'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'no' => Yii::t('app', 'No'),
+            'date' => Yii::t('app', 'Date'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'updated_by' => Yii::t('app', 'Updated By'),
+        ];
+    }
+
+    public function initNo()
+    {
+        $modal = Invoice::find()
+            ->        
+    }
+}
---
+++ views/invoice/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\InvoiceSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Invoices');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="invoice-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Invoice'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'no',
+            'date',
+            'created_at',
+            'created_by',
+            //'updated_at',
+            //'updated_by',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Invoice $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
