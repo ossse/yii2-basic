a9ac5ce7ec2aa3fff6548b1c17c202b68bc55505
Yoseph Prasetyo Lembang <yoseph.prasetyo@esb.co.id>
2022-06-13 14:40:28 +0700

    revert SiteController

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,7 +61,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('view');
+        return $this->render('index');
     }
 
     /**
